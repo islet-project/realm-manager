ef9b04037771ec576577022a52b839c92d126758
MJJ <personalmj00@gmail.com>
2024-07-25 12:28:36 +0200

    Refactor warden client library
    Signed-off-by: MJJ <personalmj00@gmail.com>

--- warden_client_lib/src/lib.rs
+++ warden_client_lib/src/lib.rs
@@ -1,31 +1,20 @@
-use std::{io, path::PathBuf};
-use thiserror::Error;
+use request_handlers::{
+    connect_to_warden_sokcet, create_application, create_realm, destroy_realm, inspect_realm,
+    list_realms, reboot_realm, start_application, start_realm, stop_application, stop_realm,
+    update_application,
+};
+use std::path::PathBuf;
 use tokio::net::UnixStream;
-use utils::serde::{JsonFramed, JsonFramedError};
+use utils::serde::JsonFramed;
 use uuid::Uuid;
 use warden_client::{
     applciation::ApplicationConfig,
-    client::{WardenCommand, WardenDaemonError, WardenResponse},
+    client::{WardenCommand, WardenResponse},
     realm::{RealmConfig, RealmDescription},
 };
-
-#[derive(Debug, Error)]
-pub enum WardenClientError {
-    #[error(
-        "Failed to connect to Warden's socket at path: {socket_path}. More details: {details}"
-    )]
-    ConnectionFailed {
-        socket_path: PathBuf,
-        #[source]
-        details: io::Error,
-    },
-    #[error("Warden operation failed: {0}")]
-    WardenOperationFail(#[from] WardenDaemonError),
-    #[error("Failed to communicate with Warden: {0}")]
-    CommunicationFail(#[from] JsonFramedError),
-    #[error("Invalid response: {response:#?}")]
-    InvalidResponse { response: WardenResponse },
-}
+use warden_client_error::WardenClientError;
+mod request_handlers;
+mod warden_client_error;
 
 pub struct WardenConnection {
     communicator: JsonFramed<UnixStream, WardenResponse, WardenCommand>,
@@ -35,130 +24,41 @@ impl WardenConnection {
     pub async fn connect(
         warden_socket_path: PathBuf,
     ) -> Result<WardenConnection, WardenClientError> {
-        let stream = UnixStream::connect(&warden_socket_path)
-            .await
-            .map_err(|err| WardenClientError::ConnectionFailed {
-                socket_path: warden_socket_path,
-                details: err,
-            })?;
+        let stream = connect_to_warden_sokcet(warden_socket_path).await?;
         Ok(WardenConnection {
             communicator: JsonFramed::<UnixStream, WardenResponse, WardenCommand>::new(stream),
         })
     }
 
     pub async fn create_realm(&mut self, config: RealmConfig) -> Result<Uuid, WardenClientError> {
-        self.communicator
-            .send(WardenCommand::CreateRealm { config })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::CreatedRealm { uuid } => Ok(uuid),
-            response => Err(self.handle_error_response(response)),
-        }
+        create_realm(&mut self.communicator, config).await
     }
 
     pub async fn start_realm(&mut self, uuid: Uuid) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::StartRealm { uuid })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        start_realm(&mut self.communicator, uuid).await
     }
 
     pub async fn stop_realm(&mut self, uuid: Uuid) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::StopRealm { uuid })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        stop_realm(&mut self.communicator, uuid).await
     }
 
     pub async fn reboot_realm(&mut self, uuid: Uuid) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::RebootRealm { uuid })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        reboot_realm(&mut self.communicator, uuid).await
     }
 
     pub async fn destroy_realm(&mut self, uuid: Uuid) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::DestroyRealm { uuid })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        destroy_realm(&mut self.communicator, uuid).await
     }
 
     pub async fn inspect_realm(
         &mut self,
         uuid: Uuid,
     ) -> Result<RealmDescription, WardenClientError> {
-        self.communicator
-            .send(WardenCommand::InspectRealm { uuid })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::InspectedRealm { description } => Ok(description),
-            response => Err(self.handle_error_response(response)),
-        }
+        inspect_realm(&mut self.communicator, uuid).await
     }
 
     pub async fn list_realms(&mut self) -> Result<Vec<RealmDescription>, WardenClientError> {
-        self.communicator
-            .send(WardenCommand::ListRealms)
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::ListedRealms { realms_description } => Ok(realms_description),
-            response => Err(self.handle_error_response(response)),
-        }
+        list_realms(&mut self.communicator).await
     }
 
     pub async fn create_application(
@@ -166,19 +66,7 @@ impl WardenConnection {
         realm_uuid: Uuid,
         config: ApplicationConfig,
     ) -> Result<Uuid, WardenClientError> {
-        self.communicator
-            .send(WardenCommand::CreateApplication { realm_uuid, config })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::CreatedApplication { uuid } => Ok(uuid),
-            response => Err(self.handle_error_response(response)),
-        }
+        create_application(&mut self.communicator, realm_uuid, config).await
     }
 
     pub async fn update_application(
@@ -187,23 +75,7 @@ impl WardenConnection {
         application_uuid: Uuid,
         config: ApplicationConfig,
     ) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::UpdateApplication {
-                realm_uuid,
-                application_uuid,
-                config,
-            })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        update_application(&mut self.communicator, realm_uuid, application_uuid, config).await
     }
 
     pub async fn start_application(
@@ -211,22 +83,7 @@ impl WardenConnection {
         realm_uuid: Uuid,
         application_uuid: Uuid,
     ) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::StartApplication {
-                realm_uuid,
-                application_uuid,
-            })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
+        start_application(&mut self.communicator, realm_uuid, application_uuid).await
     }
 
     pub async fn stop_application(
@@ -234,30 +91,6 @@ impl WardenConnection {
         realm_uuid: Uuid,
         application_uuid: Uuid,
     ) -> Result<(), WardenClientError> {
-        self.communicator
-            .send(WardenCommand::StopApplication {
-                realm_uuid,
-                application_uuid,
-            })
-            .await
-            .map_err(WardenClientError::CommunicationFail)?;
-        match self
-            .communicator
-            .recv()
-            .await
-            .map_err(WardenClientError::CommunicationFail)?
-        {
-            WardenResponse::Ok => Ok(()),
-            response => Err(self.handle_error_response(response)),
-        }
-    }
-
-    fn handle_error_response(&self, response: WardenResponse) -> WardenClientError {
-        match response {
-            WardenResponse::Error { warden_error } => {
-                WardenClientError::WardenOperationFail(warden_error)
-            }
-            response => WardenClientError::InvalidResponse { response },
-        }
+        stop_application(&mut self.communicator, realm_uuid, application_uuid).await
     }
 }
---
+++ warden_client_lib/src/request_handlers.rs
@@ -0,0 +1,192 @@
+use std::path::PathBuf;
+
+use tokio::net::UnixStream;
+use utils::serde::JsonFramed;
+use uuid::Uuid;
+use warden_client::{
+    applciation::ApplicationConfig,
+    client::{WardenCommand, WardenResponse},
+    realm::{RealmConfig, RealmDescription},
+};
+
+use crate::warden_client_error::WardenClientError;
+
+type Communicator = JsonFramed<UnixStream, WardenResponse, WardenCommand>;
+
+pub async fn create_realm(
+    communicator: &mut Communicator,
+    config: RealmConfig,
+) -> Result<Uuid, WardenClientError> {
+    match communicate(communicator, WardenCommand::CreateRealm { config }).await? {
+        WardenResponse::CreatedRealm { uuid } => Ok(uuid),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn start_realm(
+    communicator: &mut Communicator,
+    uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(communicator, WardenCommand::StartRealm { uuid }).await? {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn stop_realm(
+    communicator: &mut Communicator,
+    uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(communicator, WardenCommand::StopRealm { uuid }).await? {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn reboot_realm(
+    communicator: &mut Communicator,
+    uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(communicator, WardenCommand::RebootRealm { uuid }).await? {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn destroy_realm(
+    communicator: &mut Communicator,
+    uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(communicator, WardenCommand::DestroyRealm { uuid }).await? {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn inspect_realm(
+    communicator: &mut Communicator,
+    uuid: Uuid,
+) -> Result<RealmDescription, WardenClientError> {
+    match communicate(communicator, WardenCommand::InspectRealm { uuid }).await? {
+        WardenResponse::InspectedRealm { description } => Ok(description),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn list_realms(
+    communicator: &mut Communicator,
+) -> Result<Vec<RealmDescription>, WardenClientError> {
+    match communicate(communicator, WardenCommand::ListRealms).await? {
+        WardenResponse::ListedRealms { realms_description } => Ok(realms_description),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn create_application(
+    communicator: &mut Communicator,
+    realm_uuid: Uuid,
+    config: ApplicationConfig,
+) -> Result<Uuid, WardenClientError> {
+    match communicate(
+        communicator,
+        WardenCommand::CreateApplication { realm_uuid, config },
+    )
+    .await?
+    {
+        WardenResponse::CreatedApplication { uuid } => Ok(uuid),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn update_application(
+    communicator: &mut Communicator,
+    realm_uuid: Uuid,
+    application_uuid: Uuid,
+    config: ApplicationConfig,
+) -> Result<(), WardenClientError> {
+    match communicate(
+        communicator,
+        WardenCommand::UpdateApplication {
+            realm_uuid,
+            application_uuid,
+            config,
+        },
+    )
+    .await?
+    {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn start_application(
+    communicator: &mut Communicator,
+    realm_uuid: Uuid,
+    application_uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(
+        communicator,
+        WardenCommand::StartApplication {
+            realm_uuid,
+            application_uuid,
+        },
+    )
+    .await?
+    {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn stop_application(
+    communicator: &mut Communicator,
+    realm_uuid: Uuid,
+    application_uuid: Uuid,
+) -> Result<(), WardenClientError> {
+    match communicate(
+        communicator,
+        WardenCommand::StopApplication {
+            realm_uuid,
+            application_uuid,
+        },
+    )
+    .await?
+    {
+        WardenResponse::Ok => Ok(()),
+        response => Err(handle_error_response(response)),
+    }
+}
+
+pub async fn connect_to_warden_sokcet(
+    warden_socket_path: PathBuf,
+) -> Result<UnixStream, WardenClientError> {
+    UnixStream::connect(&warden_socket_path)
+        .await
+        .map_err(|err| WardenClientError::ConnectionFailed {
+            socket_path: warden_socket_path,
+            details: err,
+        })
+}
+
+async fn communicate(
+    communicator: &mut Communicator,
+    command: WardenCommand,
+) -> Result<WardenResponse, WardenClientError> {
+    communicator
+        .send(command)
+        .await
+        .map_err(WardenClientError::CommunicationFail)?;
+    communicator
+        .recv()
+        .await
+        .map_err(WardenClientError::CommunicationFail)
+}
+
+fn handle_error_response(response: WardenResponse) -> WardenClientError {
+    match response {
+        WardenResponse::Error { warden_error } => {
+            WardenClientError::WardenOperationFail(warden_error)
+        }
+        response => WardenClientError::InvalidResponse { response },
+    }
+}
