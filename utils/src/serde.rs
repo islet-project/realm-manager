1db2bd7262c4af4243bb4e972fa59ca218a2e01b
MJJ <personalmj00@gmail.com>
2024-07-26 10:48:37 +0200

    Write more tests for utils crate
    Signed-off-by: MJJ <personalmj00@gmail.com>

--- utils/src/serde.rs
+++ utils/src/serde.rs
@@ -63,23 +63,69 @@ impl<
 
 #[cfg(test)]
 mod test {
-    use tokio::net::UnixStream;
+    use tokio::{net::UnixStream, task::JoinHandle};
 
     #[tokio::test]
     async fn communication_through_tokio_serde() {
-        let (sock1, sock2) = UnixStream::pair().unwrap();
-        let mut communicator1 = super::JsonFramed::<UnixStream, u32, u32>::new(sock1);
-        let mut communicator2 = super::JsonFramed::<UnixStream, u32, u32>::new(sock2);
-        let task: tokio::task::JoinHandle<Result<(), super::JsonFramedError>> =
-            tokio::spawn(async move {
-                const MESSAGE: u32 = 0;
-                communicator1.send(MESSAGE).await?;
-                let result = communicator1.recv().await?;
-                assert_eq!(result, MESSAGE);
-                Ok(())
-            });
-        let message = communicator2.recv().await.unwrap();
-        communicator2.send(message).await.unwrap();
+        let (sender_stream, receiver_stream) = UnixStream::pair().unwrap();
+        let mut sender = super::JsonFramed::<UnixStream, u32, u32>::new(sender_stream);
+        let mut receiver = super::JsonFramed::<UnixStream, u32, u32>::new(receiver_stream);
+        let task: JoinHandle<Result<(), super::JsonFramedError>> = tokio::spawn(async move {
+            const MESSAGE: u32 = 0;
+            sender.send(MESSAGE).await?;
+            let result = sender.recv().await?;
+            assert_eq!(result, MESSAGE);
+            Ok(())
+        });
+        let message = receiver.recv().await.unwrap();
+        receiver.send(message).await.unwrap();
         assert!(task.await.unwrap().is_ok());
+    }
+
+    #[tokio::test]
+    async fn sender_disconnect() {
+        let (sender_stream, receiver_stream) = UnixStream::pair().unwrap();
+        let mut receiver = super::JsonFramed::<UnixStream, u32, u32>::new(receiver_stream);
+        let task: JoinHandle<()> = tokio::spawn(async move {
+            let _ = super::JsonFramed::<UnixStream, u32, u32>::new(sender_stream);
+        });
+        assert!(match receiver.recv().await {
+            Err(super::JsonFramedError::StreamIsClosed()) => Ok(()),
+            _ => Err(()),
+        }
+        .is_ok());
+        task.await.unwrap()
+    }
+
+    #[tokio::test]
+    async fn receive_error() {
+        let (sender_stream, receiver_stream) = UnixStream::pair().unwrap();
+        let mut sender = super::JsonFramed::<UnixStream, String, String>::new(sender_stream);
+        let mut receiver = super::JsonFramed::<UnixStream, f32, f32>::new(receiver_stream);
+        let task: JoinHandle<Result<(), super::JsonFramedError>> = tokio::spawn(async move {
+            sender.send(String::from("")).await?;
+            Ok(())
+        });
+        assert!(match receiver.recv().await {
+            Err(super::JsonFramedError::SerdeReadError(_)) => Ok(()),
+            _ => Err(()),
+        }
+        .is_ok());
+        assert!(task.await.unwrap().is_ok());
+    }
+
+    #[tokio::test]
+    async fn receiver_disconnect() {
+        let (sender_stream, receiver_stream) = UnixStream::pair().unwrap();
+        let mut sender = super::JsonFramed::<UnixStream, u32, u32>::new(sender_stream);
+        let task: JoinHandle<()> = tokio::spawn(async move {
+            let _ = super::JsonFramed::<UnixStream, u32, u32>::new(receiver_stream);
+        });
+        assert!(match sender.send(0).await {
+            Err(super::JsonFramedError::SerdeWriteError(_)) => Ok(()),
+            _ => Err(()),
+        }
+        .is_ok());
+        task.await.unwrap()
     }
 }
